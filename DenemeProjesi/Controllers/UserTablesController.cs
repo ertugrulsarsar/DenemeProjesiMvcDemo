07eb3838d7f6ea68d7f219a3a8703c1b4e582dee
Ertugrul Sarsar <ertugrulsarsar@gmail.com>
2019-10-01 18:43:34 +0300

    Add project files.

---
+++ DenemeProjesi/Controllers/UserTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DenemeProjesi.Models;
+
+namespace DenemeProjesi.Controllers
+{
+    public class UserTablesController : Controller
+    {
+        private MVCDemoEntities db = new MVCDemoEntities();
+
+        // GET: UserTables
+        public ActionResult Index()
+        {
+            return View(db.UserTable.ToList());
+        }
+
+        // GET: UserTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,AvatarURL,FirstName,LastName,CompanyName,ContactPhone,EmailAddess,CompanySite")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTable.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTable);
+        }
+
+        // GET: UserTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,AvatarURL,FirstName,LastName,CompanyName,ContactPhone,EmailAddess,CompanySite")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTable);
+        }
+
+        // GET: UserTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserTable userTable = db.UserTable.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: UserTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserTable userTable = db.UserTable.Find(id);
+            db.UserTable.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
